413c7019d214fea3f30c8c25b1a23071f886276b
Zineddine Zahouani <43182889+zineddinezino@users.noreply.github.com>
2022-10-03 23:33:54 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentsListComponent } from './students-list/students-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'students', component: StudentsListComponent},
+  {path: '', redirectTo: 'students', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
