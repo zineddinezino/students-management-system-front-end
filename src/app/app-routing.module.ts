f0b7f34c18da0ad44f3ff281807a356f9a5e50a5
Zineddine Zahouani <43182889+zineddinezino@users.noreply.github.com>
2022-10-04 23:17:20 +0200

    Adding new Students component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateStudentComponent } from './create-student/create-student.component';
 import { StudentsListComponent } from './students-list/students-list.component';
 
 const routes: Routes = [
   {path: 'students', component: StudentsListComponent},
+  {path: 'add-student', component:CreateStudentComponent},
   {path: '', redirectTo: 'students', pathMatch: 'full'}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentsListComponent } from './students-list/students-list.component';
+import { CreateStudentComponent } from './create-student/create-student.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentsListComponent
+    StudentsListComponent,
+    CreateStudentComponent
   ],
   imports: [
     BrowserModule,
