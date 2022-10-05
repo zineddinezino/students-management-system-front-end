17563209c6bf5057f8d1c7ea049535ebb1a17cae
Zineddine Zahouani <43182889+zineddinezino@users.noreply.github.com>
2022-10-05 21:09:38 +0200

    another student model for the posts requests

---
+++ src/app/model/AddStudent/add-student.model.spec.ts
@@ -0,0 +1,7 @@
+import { AddStudent } from './add-student.model';
+
+describe('AddStudent', () => {
+  it('should create an instance', () => {
+    expect(new AddStudent()).toBeTruthy();
+  });
+});
---
+++ src/app/model/AddStudent/add-student.model.ts
@@ -0,0 +1,8 @@
+export class AddStudent {
+    studentId: number | undefined;
+    firstName: string | undefined;
+    lastName: string | undefined;
+    email:string | undefined;
+    password:string | undefined;
+    dateOfBirth: string | undefined;
+}
